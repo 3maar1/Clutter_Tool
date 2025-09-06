9f8532bf0d10d34908f2b6b20166a0cc5c749db4
3maar1 <amarmostafaabdelhady265@gmail.com>
2025-09-06 12:47:24 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+from colorama import init,Fore
+init(autoreset=True)
+while True:
+    print(Fore.BLUE +
+    """"
+    Welcome to personal assistant,
+    ██████╗ ██╗      █████╗ ███╗   ██╗██╗██╗  ██╗
+    ██╔══██╗██║     ██╔══██╗████╗  ██║██║╚██╗██╔╝
+    ██████╔╝██║     ███████║██╔██╗ ██║██║ ╚███╔╝ 
+    ██╔═══╝ ██║     ██╔══██║██║╚██╗██║██║ ██╔██╗ 
+    ██║     ███████╗██║  ██║██║ ╚████║██║██╔╝ ██╗
+    ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝
+
+    Designed to declutter you time and simplify your day with daily essential tool that enhances your productivity!
+    Features:
+            1- Activity Tracking,
+            2- Budget Management,
+            3- To Do List,
+            4- Pomodoro Timer,
+            5- Minigame.
+    """)
+    choice = input(Fore.GREEN + "Select a feature (1-5):")
+    if (choice == "1"):
+        while True:
+            quit = input("")
+
+            if (quit == "q"):
+                break
+
+    elif (choice == "2"):
+        while True:
+            quit = input("")
+
+            if (quit == "q"):
+                break
+
+    elif (choice == "3"):
+        while True:
+            quit = input("")
+
+            if (quit == "q"):
+                break
+
+    elif (choice == "4"):
+        while True:
+            quit = input("")
+
+            if (quit == "q"):
+                break
+
+    elif (choice == "5"):
+        while True:
+            quit = input("")
+
+            if (quit == "q"):
+                break;
+    elif (choice == "q"):
+        exit()
